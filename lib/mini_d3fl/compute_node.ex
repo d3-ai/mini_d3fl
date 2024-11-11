faa3c6247530af12954aec246cb97d1b28692575
up0pt <giftmath01au@gmail.com>
2024-11-12 00:54:19 +0900

    ADD compute_node test & compute_node

--- lib/mini_d3fl/compute_node.ex
+++ lib/mini_d3fl/compute_node.ex
@@ -2,18 +2,26 @@ defmodule MiniD3fl.ComputeNode do
   require Logger
   use GenServer
   alias MiniD3fl.Utils
+  alias MiniD3fl.Channel
+
+  defmodule Model do
+    defstruct size: nil,
+              plain_model: nil
+  end
 
   defmodule State do
     defstruct node_id: nil,
-              now_model: nil,
-              future_model: nil,
-              data: nil
+              now_model: %Model{},
+              future_model: %Model{},
+              data: nil,
+              cn_id_channel_pid_dict: %{}
   end
 
   defmodule InitArgs do
     defstruct node_id: nil,
-              model: nil,
-              data: nil
+              model: %Model{},
+              data: nil,
+              cn_id_channel_pid_dict: %{}
   end
 
   defmodule TrainArgs do
@@ -42,7 +50,8 @@ defmodule MiniD3fl.ComputeNode do
     %InitArgs{
       node_id: node_id,
       model: model,
-      data: data
+      data: data,
+      cn_id_channel_pid_dict: cn_id_channel_pid_dict
     }) do
 
     {:ok,
@@ -50,15 +59,47 @@ defmodule MiniD3fl.ComputeNode do
       node_id: node_id,
       now_model: model,
       future_model: nil,
-      data: data
+      data: data,
+      cn_id_channel_pid_dict: cn_id_channel_pid_dict
     }}
   end
 
-  #def send_model(send_node_id, recv_node_id) do
-  #  GenServer.call(
-  #    Utils.get_process_name(__MODULE__, send_node_id),
-  #    {:send_model, recv_node_id}
-  #    )
-  #end
+  def train(%TrainArgs{node_id: node_id} = args) do
+    GenServer.call(
+      Utils.get_process_name(__MODULE__, node_id),
+      {:train, args}
+    )
+  end
+
+  def send_to_channel(%SendArgs{from_node_id: from_node_id} = send_args) do
+    GenServer.call(
+      Utils.get_process_name(__MODULE__, from_node_id),
+      {:send_to_channel, send_args}
+      )
+  end
+
+  def handle_call({:train,
+                    args},
+                  _from,
+                  %State{node_id: node_id} = state) do
+    IO.puts "Node id: #{node_id} in TRAIN"
+    # TODO: Trainに書き換える
+    new_model = nil
+    train_results = :train_results
+    # TODO: Trainした時の結果に書き換える
+    {:reply, train_results, %State{state | future_model: new_model}}
+  end
+
+  def handle_call({:send_to_channel,
+                    %SendArgs{
+                      channel_pid: channel_pid
+                    }} = _send_args,
+                    _from,
+                    %State{now_model: now_model} = state) do
+
+    Channel.recv_model_at_channel(channel_pid, now_model)
+    IO.puts "sent to channel pid #{channel_pid}"
+    {:reply, nil, state}
+  end
 
 end
--- test/mini_d3fl/compute_node_test.exs
+++ test/mini_d3fl/compute_node_test.exs
@@ -1,16 +1,29 @@
 defmodule MiniD3fl.ComputeNodeTest do
   use ExUnit.Case
   doctest MiniD3fl.ComputeNode
+  alias MiniD3fl.ComputeNode.TrainArgs
   alias MiniD3fl.ComputeNode.InitArgs
   alias MiniD3fl.ComputeNode
 
   test "should init with proper state" do
-    args = %ComputeNode.InitArgs{node_id: nil,
+    args = %InitArgs{node_id: nil,
                       model: nil,
                       data: nil}
 
-    {value, pid}  = ComputeNode.start_link(args)
+    {value, _pid}  = ComputeNode.start_link(args)
     assert value == :ok
+  end
+
+  test "should train with proper state" do
+    args = %InitArgs{node_id: 1,
+                      model: nil,
+                      data: nil}
+
+    {value, _pid}  = ComputeNode.start_link(args)
+    assert value == :ok
+
+    train_results = ComputeNode.train(%TrainArgs{node_id: 1})
+    assert train_results == :train_results
   end
 
 end
