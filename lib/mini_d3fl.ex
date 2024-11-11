0e37e421c46c523484759cf54f93f06ef03e157d
up0pt <giftmath01au@gmail.com>
2024-11-12 00:53:54 +0900

    ADD memo for test in MiniD3fl

--- lib/mini_d3fl.ex
+++ lib/mini_d3fl.ex
@@ -1,5 +1,17 @@
 defmodule MiniD3fl do
+  use ExUnit.Case
   @moduledoc """
   Documentation for `MiniD3fl`.
   """
+
+  test "should deal with stucked model to the same cn" do
+
+    #例えば、
+    #CN1 <-[]-> CN2
+    #において、それぞれが、モデルを頻繁に送るために、モデルがスタックする場合。
+    #チャンネルでモデルの上書きがない＆チャンネル容量を超えるとなくなる＆順にCN2などが更新される
+    #を確認したい。
+
+    assert false
+  end
 end
