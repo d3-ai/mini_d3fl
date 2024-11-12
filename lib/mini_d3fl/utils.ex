719f19e968d73493ee81d8f04edaae39d2ab5a77
up0pt <giftmath01au@gmail.com>
2024-11-12 13:44:44 +0900

    Revise warning comments

--- lib/mini_d3fl/utils.ex
+++ lib/mini_d3fl/utils.ex
@@ -1,10 +1,8 @@
 defmodule MiniD3fl.Utils do
   def get_process_name(module, index) do
-    """
-    JobController と ComputerNodeにのみ使う。
-    Channelには使わない。
-    これで、並列の理論的限界は100,00,00程度。
-    """
+    # JobController と ComputerNodeにのみ使う。
+    # Channelには使わない。
+    # これで、並列の理論的限界は100,00,00程度。
     :"#{module}_#{index}"
   end
 
