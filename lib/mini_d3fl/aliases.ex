cbb50049112ad9893841589f61feecfe8e2f2ef6
up0pt <giftmath01au@gmail.com>
2024-11-15 00:07:13 +0900

    Add aliases file

---
+++ lib/mini_d3fl/aliases.ex
@@ -0,0 +1,14 @@
+defmodule MiniD3fl.Aliases do
+  defmacro __using__(_) do
+    quote do
+      alias MiniD3fl.ComputeNode
+      alias MiniD3fl.ComputeNode.{Model, InitArgs, TrainArgs, SendArgs, RecvArgs, RenewModelArgs}
+      alias MiniD3fl.Utils
+      alias MiniD3fl.Channel
+      alias MiniD3fl.Channel.{ChannelArgs, QoS}
+      alias MiniD3fl.JobController
+      alias MiniD3fl.JobController.EventQueue
+      alias MiniD3fl.JobController.EventQueue.Event
+    end
+  end
+end
