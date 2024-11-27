0e2da46b0bec481ecc8d56d71ea7fd2c6ef5351a
up0pt <giftmath01au@gmail.com>
2024-11-27 10:57:49 +0900

    Add Mnist AI Core

---
+++ lib/mini_d3fl/compute_node/ai_core.ex
@@ -0,0 +1,14 @@
+defmodule MiniD3fl.ComputeNode.AiCore do
+  use GenServer
+  alias Mnist
+
+  def run() do
+    # train and run
+    # sample
+    Mnist.run()
+  end
+
+  def aggregate() do
+
+  end
+end
