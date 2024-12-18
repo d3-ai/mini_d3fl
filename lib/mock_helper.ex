a003106dc586e718365e3cf182b4d1fb294b4881
up0pt <giftmath01au@gmail.com>
2024-12-19 00:23:31 +0900

    Init connect mock [WIP]

--- lib/mock_helper.ex
+++ lib/mock_helper.ex
@@ -1,3 +1,19 @@
 defmodule MockHelper do
   use MiniD3fl.Aliases
+
+  def prepare_data_directory!(node_counts, name) do
+    data_directory_path =
+      Application.get_env(:mini_d3fl, :data_directory_path) ||
+        raise """
+        You have to configure :data_directory_path in config.exs
+        ex) config :mini_d3fl, :data_directory_path, "path/to/directory"
+        """
+
+    dt_string = Data.datetime_to_string(DateTime.utc_now())
+    directory_name = "date_#{dt_string}_#{name}_CalculatorNodeNum_#{node_counts}"
+    data_directory_path = Path.join(data_directory_path, directory_name)
+
+    File.mkdir_p!(data_directory_path)
+    data_directory_path
+  end
 end
